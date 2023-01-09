3266dd1c569fab1a512e1d9e08687acbe1462d37
Elliot Pettingale <elliot@smartercoding.co.uk>
2023-01-09 11:28:16 +0000

    extract search logic to separate component

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,18 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
-import Select from "./common/Select";
-import Button from "./common/Button";
 import Result from "./common/Result";
-import { getBreeds, getSubBreeds } from "./services/dogApiService";
+import Controls from "./components/Controls";
 
 function App() {
-  const [breeds, setBreeds] = useState<any>([]);
-  const [subBreeds, setSubBreeds] = useState<string[]>([]);
-
-  const [selectedBreed, setSelectedBreed] = useState<string>("");
-  const [selectedSubBreed, setSelectedSubBreed] = useState<string>("");
-  const [selectedNumberOfImages, setSelectedNumberOfImages] =
-    useState<string>("");
-
-  useEffect(() => {
-    getBreeds().then((breeds) => {
-      setBreeds(breeds);
-    });
-  }, []);
-
-  useEffect(() => {
-    if (selectedBreed === "") {
-      setSubBreeds([]);
-    } else {
-      getSubBreeds(selectedBreed).then((subBreeds) => {
-        setSubBreeds(subBreeds);
-      });
-    }
-  }, [selectedBreed]);
+  const handleViewImages = (selections: Object) => {
+    console.log("VIEW IMAGES");
+  };
 
   return (
     <div className="App">
-      <div className="App__Selects">
-        <Select
-          options={breeds}
-          value={selectedBreed}
-          setSelected={(value) => setSelectedBreed(value)}
-        />
-        <Select
-          options={subBreeds}
-          value={selectedSubBreed}
-          setSelected={(value) => setSelectedSubBreed(value)}
-        />
-        <Select
-          options={["1", "2", "3", "4", "5", "6", "7", "8"]}
-          value={selectedNumberOfImages}
-          setSelected={(value) => setSelectedNumberOfImages(value)}
-        />
-        <Button
-          text="View Images"
-          disabled={false}
-          onClick={() => {
-            console.log("click");
-          }}
-        />
-      </div>
-      <div className="debug--remove">
-        <p>Breed: {selectedBreed}</p>
-        <p>SubBreed: {selectedSubBreed}</p>
-        <p>NumberOfImages: {selectedNumberOfImages}</p>
-      </div>
+      <Controls
+        handleViewImages={(selections) => handleViewImages(selections)}
+      />
       <div className="App__Results">
         <Result image="https://images.dog.ceo/breeds/hound-afghan/n02088094_1003.jpg" />
         <Result image="https://images.dog.ceo/breeds/hound-afghan/n02088094_1007.jpg" />
---
+++ src/components/Controls/index.tsx
@@ -0,0 +1,74 @@
+import React, { useState, useEffect } from "react";
+import Select from "../../common/Select";
+import Button from "../../common/Button";
+import { getBreeds, getSubBreeds } from "../../services/dogApiService";
+
+interface Props {
+  handleViewImages: ({}) => void;
+}
+
+function Controls({ handleViewImages }: Props) {
+  const [breeds, setBreeds] = useState<any>([]);
+  const [subBreeds, setSubBreeds] = useState<string[]>([]);
+
+  const [selectedBreed, setSelectedBreed] = useState<string>("");
+  const [selectedSubBreed, setSelectedSubBreed] = useState<string>("");
+  const [selectedNumberOfImages, setSelectedNumberOfImages] =
+    useState<string>("");
+
+  useEffect(() => {
+    getBreeds().then((breeds) => {
+      setBreeds(breeds);
+    });
+  }, []);
+
+  useEffect(() => {
+    if (selectedBreed === "") {
+      setSubBreeds([]);
+    } else {
+      getSubBreeds(selectedBreed).then((subBreeds) => {
+        setSubBreeds(subBreeds);
+      });
+    }
+
+    setSelectedSubBreed("");
+  }, [selectedBreed]);
+
+  return (
+    <div className="App__Controls">
+      <Select
+        options={breeds}
+        value={selectedBreed}
+        setSelected={(value) => setSelectedBreed(value)}
+      />
+      <Select
+        options={subBreeds}
+        value={selectedSubBreed}
+        setSelected={(value) => setSelectedSubBreed(value)}
+      />
+      <Select
+        options={["1", "2", "3", "4", "5", "6", "7", "8"]}
+        value={selectedNumberOfImages}
+        setSelected={(value) => setSelectedNumberOfImages(value)}
+      />
+      <Button
+        text="View Images"
+        disabled={false}
+        onClick={() =>
+          handleViewImages({
+            breed: selectedBreed,
+            subBreed: selectedSubBreed,
+            numberOfImages: selectedNumberOfImages,
+          })
+        }
+      />
+      <div className="debug--remove">
+        <p>Breed: {selectedBreed}</p>
+        <p>SubBreed: {selectedSubBreed}</p>
+        <p>NumberOfImages: {selectedNumberOfImages}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Controls;
